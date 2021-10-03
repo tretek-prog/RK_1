ac3c758bf274b0a51409157174ebc862c500f396
tretek-prog <70658598+tretek-prog@users.noreply.github.com>
2021-10-03 20:57:24 +0200

    Update 3.py

--- code_tasks/3.py
+++ code_tasks/3.py
@@ -1,9 +1,9 @@
 import json
 import random
 import requests
-
 from http.server import BaseHTTPRequestHandler
 from helpers import http_server
+from domonic import *
 
 # urllib3 for url parse
 
@@ -13,10 +13,30 @@ class BookHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         print('Get request received')
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        resp = requests.get(f"http://numbersapi.com/{random.randint(0, 100)}")
-        self.wfile.write(json.dumps({'status': resp.status_code, 'content': resp.content.decode()}).encode())
+        r = random.randint(0, 100)
+        resp = requests.get(f"http://numbersapi.com/{r}")
+        respon = render(
+            html(
+                head(
+                ),
+                body(
+                    style(
+                        'BODY {background: url(https://capital2020.ru/d/mgtu_im_n_e_baumana_moskva.jpg) no-repeat;}',
+                        _type="text/css",
+                        ),
+                    header(b('Random interesting fact about ', r,
+                    _style =
+                    '''font-size: 30px;
+                    color: green;
+                    ''')),
+                    p(resp.content.decode())
+                )
+            )
+        )
+        self.wfile.write(respon.encode())
+
 
 
 if __name__ == "__main__":
